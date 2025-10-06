17ae186f8ad6a7b14890fd4ffb1162884a645aec
Teemu Erkkilä <teemu.erkkila@tuni.fi>
2025-10-06 20:49:38 +0300

    Auctionmanager to set added item to auction immediately

--- src/main/java/club/nullbyte3/auction/impl/AuctionManager.java
+++ src/main/java/club/nullbyte3/auction/impl/AuctionManager.java
@@ -20,7 +20,7 @@ public class AuctionManager {
     private static final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     private static volatile ScheduledFuture<?> endTask;
-    private static final int AUCTION_DURATION = 15;
+    private static final int AUCTION_DURATION = 25;
     private static BidManager bidManager;
 
     public static void start(BidManager bidManager) {
@@ -30,6 +30,9 @@ public class AuctionManager {
 
     public static void addItem(Item item) {
         itemQueue.add(item);
+        if(currentItem == null) {
+            nextItem();
+        }
     }
 
     private static void nextItem() {
